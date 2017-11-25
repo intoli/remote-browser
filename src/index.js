e2626fdfb9c5a43065d47b90cbf8c09e01ce802d
Evan Sangaline <esangaline@gmail.com>
2017-11-25 13:42:30 -0500

    Add missing return statement.

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import FeverDreamChrome from './chrome';
 export default async (options) => {
   const feverDream = new FeverDreamChrome(options);
   await feverDream.initialize();
+  return feverDream;
 };
