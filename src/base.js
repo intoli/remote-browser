9556406d387b7fb3c0437e4df44b1f73cb7b3c1d
Evan Sangaline <esangaline@gmail.com>
2017-11-25 13:41:03 -0500

    Add a default implementation of `end()` for Selenium clients.

--- src/base.js
+++ src/base.js
@@ -2,4 +2,9 @@ export default class FeverDreamBase {
   constructor(options) {
     this.options = options;
   }
+
+  end = async () => {
+    // Works for Selenium driven clients.
+    await this.driver.quit();
+  };
 }
