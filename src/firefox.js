93092cc55d87ae58e18f17e622addc6eb6227ce4
Evan Sangaline <esangaline@gmail.com>
2017-11-25 12:43:42 -0500

    Switch to selenium from marionette.

--- src/firefox.js
+++ src/firefox.js
@@ -1,30 +1,20 @@
-import Marionette from 'marionette-client';
+import path from 'path';
+
+import { Builder } from 'selenium-webdriver';
 
 import FeverDreamBase from './base';
 
 
 export default class FeverDreamFirefox extends FeverDreamBase {
-  initialize = async url => new Promise((resolve, revoke) => {
-    const driver = new Marionette.Drivers.Tcp({});
-    driver.connect((connectionError) => {
-      if (connectionError) {
-        revoke(connectionError);
-        return;
-      }
-      const client = new Marionette.Client(driver);
-      client.startSession((startError) => {
-        if (startError) {
-          revoke(startError);
-          return;
-        }
-        client.goUrl(url, (goError) => {
-          if (goError) {
-            revoke(goError);
-            return;
-          }
-          resolve(client);
-        });
-      });
-    });
-  });
+  // TODO: This functionality is on master, but hasn't been released yet.
+  // We should revisit this in a few weeks.
+  // https://github.com/SeleniumHQ/selenium/commit/6648015ba9490c5971ea2d6b80908f81ae6c7839
+  initialize = async (url) => {
+    this.driver = await new Builder()
+      .forBrowser('firefox')
+      .build();
+    await this.driver.installAddon(path.join(__dirname, 'extension'));
+    await this.driver.get(url);
+    return this;
+  }
 }
