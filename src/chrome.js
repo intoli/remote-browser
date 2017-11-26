1ad8df3aee1baf77e8c89929ebba639457a367af
Evan Sangaline <esangaline@gmail.com>
2017-11-26 13:00:32 -0500

    Add a first pass of connecting from within the extension.

--- src/chrome.js
+++ src/chrome.js
@@ -4,21 +4,28 @@ import { Builder } from 'selenium-webdriver';
 import chrome from 'selenium-webdriver/chrome';
 
 import FeverDreamBase from './base';
+import { Server } from './connections';
 
 
 export default class FeverDreamChrome extends FeverDreamBase {
-  initialize = async (url) => {
+  initialize = async () => {
     const extensionPath = path.resolve(__dirname, 'extension');
-    console.log(__dirname);
-    console.log(extensionPath);
     this.driver = await new Builder()
       .forBrowser('chrome')
       .setChromeOptions(new chrome.Options()
         .addArguments([`--load-extension=${extensionPath}`]))
       .build();
-    if (url) {
+
+    return new Promise(async (resolve) => {
+      this.server = new Server();
+      this.server.once('connect', resolve);
+      this.port = await this.server.listen();
+
+      const url = `file:///?feverDreamPort=${this.port}`;
       await this.driver.get(url);
-    }
-    return this;
+    }).then(async () => {
+      await this.driver.get('about:blank');
+      return this;
+    });
   }
 }
--- src/extension/background.js
+++ src/extension/background.js
@@ -1,1 +1,22 @@
-console.log('hello from the background script');
+import { Client } from '../connections/client';
+
+
+const findPort = async () => (new Promise((resolve) => {
+  const extractPort = (tabId, changeInfo) => {
+    const match = /feverDreamPort=(\d+)/.exec(changeInfo.url);
+    if (match && match.length > 1) {
+      const port = match[1];
+      resolve(port);
+      browser.tabs.onUpdated.removeListener(extractPort);
+    }
+  };
+  browser.tabs.onUpdated.addListener(extractPort);
+}));
+
+(async () => {
+  const port = await findPort();
+  console.log(`Port: ${port}`);
+  const client = new Client();
+  await client.connect(port);
+  console.log('connected!');
+})();
