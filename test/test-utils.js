df4217e11dfb9036905532bf620acb4c3f914405
Evan Sangaline <esangaline@gmail.com>
2017-11-27 18:35:43 -0500

    Add tests for TimeoutPromise.

---
+++ test/test-utils.js
@@ -0,0 +1,23 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+import * as errors from '../src/errors';
+import * as utils from '../src/utils';
+
+
+chai.use(chaiAsPromised);
+
+
+describe('utils', () => {
+  describe('TimeoutPromise', () => {
+    it('should throw an exception after timeout', () => {
+      const promise = new utils.TimeoutPromise(() => null, 50);
+      chai.expect(promise).to.be.rejectedWith(errors.TimeoutError);
+    });
+    it('should resolve with the correct value', () => {
+      const value = 'hello';
+      const promise = new utils.TimeoutPromise(resolve => resolve(value), 50);
+      chai.expect(promise).to.eventually.equal(value);
+    });
+  });
+});
