3d98cfe9427d67d1fac493f92923a62271ee7681
Evan Sangaline <esangaline@gmail.com>
2017-11-29 15:21:32 -0500

    Add a connection error.

--- src/errors.js
+++ src/errors.js
@@ -1,3 +1,12 @@
+export class ConnectionError extends Error {
+  constructor(...params) {
+    super(...params);
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ConnectionError);
+    }
+  }
+}
+
 export class TimeoutError extends Error {
   constructor(...params) {
     super(...params);
