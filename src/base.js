d300b08ab36c9ecbefdc80fd41d6fc2a81195319
Evan Sangaline <esangaline@gmail.com>
2017-11-29 21:22:47 -0500

    Refactor some of the server logic into the base class.

--- src/base.js
+++ src/base.js
@@ -1,7 +1,24 @@
+import path from 'path';
+
+import { Server } from './connections';
+
+
 export default class FeverDreamBase {
   constructor(options) {
     this.options = options;
   }
+
+  initialize = async () => {
+    const extension = path.resolve(__dirname, 'extension');
+    return new Promise(async (resolve) => {
+      this.server = new Server();
+      this.server.once('connection', resolve);
+      this.port = await this.server.listen();
+      const url = `file:///?feverDreamPort=${this.port}`;
+
+      await this.initializeDriver(url, extension);
+    });
+  };
 
   end = async () => {
     // Works for Selenium driven clients.
