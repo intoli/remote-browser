96dae6102254ff8d15837c2c082482ef35a3b525
Evan Sangaline <esangaline@gmail.com>
2018-03-08 13:18:03 -0500

    Add the start of a `Proxy` class.

---
+++ src/connections/proxy.js
@@ -0,0 +1,23 @@
+import Server from './server';
+
+
+export default class Proxy {
+  constructor() {
+    this.servers = [
+      new Server(),
+      new Server(),
+    ];
+  }
+
+  close = async () => {
+    await this.servers[0].close();
+    await this.servers[1].close();
+  };
+
+  listen = async () => {
+    // This is done sequentially for now to avoid port conflicts.
+    this.ports = [await this.servers[0].listen()];
+    this.ports.push(await this.servers[0].listen());
+    return this.ports;
+  };
+}
