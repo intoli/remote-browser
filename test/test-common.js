a5f4a2014cadfb8a48e612f28e903fc773468e79
Evan Sangaline <esangaline@gmail.com>
2018-03-08 14:31:42 -0500

    Strip out the chai stuff from the common module tests.

--- test/test-common.js
+++ test/test-common.js
@@ -1,28 +1,36 @@
-import chai from 'chai';
-import chaiAsPromised from 'chai-as-promised';
+import assert from 'assert';
 
 import * as errors from '../src/errors';
 import * as common from '../src/common';
 
-
-chai.use(chaiAsPromised);
-
+const catchError = async(promise) => {
+  try {
+    await promise;
+  } catch (e) {
+    return e;
+  }
+};
 
 describe('common', () => {
   describe('TimeoutPromise', () => {
-    it('should throw an exception after timeout', () => {
-      const promise = new common.TimeoutPromise(() => null, 50);
-      chai.expect(promise).to.be.rejectedWith(errors.TimeoutError);
+    it('should throw an exception after timeout', async () => {
+      const error = await catchError(new common.TimeoutPromise(() => null, 50))
+      assert(error instanceof errors.TimeoutError);
     });
-    it('should resolve with the correct value', () => {
-      const value = 'hello';
-      const promise = new common.TimeoutPromise(resolve => resolve(value), 50);
-      chai.expect(promise).to.eventually.equal(value);
+
+    it('should resolve with the correct value', async () => {
+      const expectedValue = 'hello';
+      const value = await new common.TimeoutPromise(resolve => resolve(expectedValue), 50);
+      assert.equal(value, expectedValue);
     });
-    it('should revoke with the correct value', () => {
-      const value = 'hello';
-      const promise = new common.TimeoutPromise((resolve, revoke) => revoke(new Error(value)), 50);
-      chai.expect(promise).to.be.rejectedWith(Error, value);
+
+    it('should revoke with the correct error message', async () => {
+      const expectedMessage = 'hello';
+      const error = await catchError(
+        new common.TimeoutPromise((resolve, revoke) => revoke(new Error(expectedMessage)), 50),
+      );
+      assert(error instanceof Error);
+      assert.equal(error.message, expectedMessage);
     });
   });
 });
