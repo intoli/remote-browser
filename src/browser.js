51a96d6008f857bf19bd43824139742cb5ff2a73
Evan Sangaline <esangaline@gmail.com>
2018-03-08 15:58:34 -0500

    Add a basic `Browser` class that can handle launching Chrome and connecting.

---
+++ src/browser.js
@@ -0,0 +1,39 @@
+import assert from 'assert';
+
+import { Client, Proxy } from './connections';
+import launchChrome from './launchers';
+
+
+export default class Browser {
+  constructor(options) {
+    this.options = options;
+  }
+
+  launch = async (browser = 'chrome') => {
+    assert(browser === 'chrome', 'Only Chrome is supported right now.');
+
+    // Set up the proxy and connect to it.
+    this.proxy = new Proxy();
+    this.ports = await this.proxy.listen();
+    this.client = new Client();
+    await this.client.connect(this.ports[0]);
+
+    // Prepare for the initial connection from the browser.
+    const connectionPromise = new Promise((resolve) => {
+      const channel = 'initialConnection';
+      const handleInitialConnection = () => {
+        console.log('unsubscript?');
+        this.client.unsubscribe(handleInitialConnection, { channel });
+        console.log('done');
+        resolve();
+      };
+      this.client.subscribe(handleInitialConnection, { channel });
+    });
+
+    // Launch the browser with the correct arguments.
+    const url = `file:///?remoteBrowserPort=${this.ports[1]}`;
+    this.driver = await launchChrome(url);
+
+    await connectionPromise;
+  };
+}
