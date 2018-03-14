dcb048abe1dfda6868ab0f7122c08ef96928bd85
Evan Sangaline <esangaline@gmail.com>
2018-03-13 20:16:27 -0400

    Refactor the background script code into a class.

--- src/extension/background.js
+++ src/extension/background.js
@@ -1,30 +1,46 @@
 import Client from '../connections/client';
 
 
-const findPort = async () => (new Promise((resolve) => {
-  const extractPort = (tabId, changeInfo, tab) => {
-    const url = tab ? tab.url : tabId;
-    const match = /remoteBrowserPort=(\d+)/.exec(url);
-    if (match && match.length > 1) {
-      const port = match[1];
-      resolve(port);
-      browser.tabs.onUpdated.removeListener(extractPort);
-      browser.tabs.update({ url: 'about:blank' });
-    }
+class Background {
+  constructor() {
+    this.client = new Client();
+
+    this.client.subscribe(async ({ args, asyncFunction }) => (
+      // eslint-disable-next-line no-eval
+      eval(`(${asyncFunction}).apply(null, ${JSON.stringify(args)})`)
+    ), { channel: 'evaluateInBackground' });
+  }
+
+  connect = async (port, host = 'ws://localhost') => {
+    await this.client.connect(port, host);
+    this.client.send(null, { channel: 'initialConnection' });
   };
-  browser.tabs.onUpdated.addListener(extractPort);
-  browser.tabs.getCurrent().then(extractPort);
-}));
+
+  connectOnLaunch = async () => {
+    const port = await this.findPort();
+    await this.connect(port);
+  };
+
+  findPort = async () => (new Promise((resolve) => {
+    const extractPort = (tabId, changeInfo, tab) => {
+      const url = tab ? tab.url : tabId;
+      const match = /remoteBrowserPort=(\d+)/.exec(url);
+      if (match && match.length > 1) {
+        const port = match[1];
+        resolve(port);
+        browser.tabs.onUpdated.removeListener(extractPort);
+        browser.tabs.update({ url: 'about:blank' });
+      }
+    };
+    browser.tabs.onUpdated.addListener(extractPort);
+    browser.tabs.getCurrent().then(extractPort);
+  }));
+}
 
 
 (async () => {
-  const port = await findPort();
-  const client = new Client();
-  await client.connect(port);
-  client.send(null, { channel: 'initialConnection' });
-
-  client.subscribe(async ({ args, asyncFunction }) => (
-    // eslint-disable-next-line no-eval
-    eval(`(${asyncFunction}).apply(null, ${JSON.stringify(args)})`)
-  ), { channel: 'evaluateInBackground' });
+  const background = new Background();
+  // TODO: This should be disabled in extension builds that are meant to be distributed
+  // independently from the node module as a security measure.
+  await background.connectOnLaunch();
 })();
