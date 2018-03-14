da654937656e3738e571c01a2b005e0ddc8e1ed1
Evan Sangaline <esangaline@gmail.com>
2018-03-13 20:59:23 -0400

    Make connection requests from the popup when the button is clicked.

--- src/extension/popup.js
+++ src/extension/popup.js
@@ -1,1 +1,13 @@
-document.body.innerHTML = 'Content injected successfully.';
+const connectButton = document.getElementById('connect');
+const hostInput = document.getElementById('host');
+const portInput = document.getElementById('port');
+
+
+connectButton.addEventListener('click', () => {
+  document.body.innerHTML += 'click!';
+  browser.runtime.sendMessage({
+    channel: 'connectionRequest',
+    host: hostInput.value,
+    port: parseInt(portInput.value, 10),
+  });
+});
