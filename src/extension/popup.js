98590791b58eda204a9617a9a71875e32f45344b
Evan Sangaline <esangaline@gmail.com>
2018-03-14 09:53:05 -0400

    Display different popups contents depending on the connection status.

--- src/extension/popup.js
+++ src/extension/popup.js
@@ -1,10 +1,23 @@
 const connectButton = document.getElementById('connect');
 const hostInput = document.getElementById('host');
 const portInput = document.getElementById('port');
+const statusContainerDiv = document.getElementById('status-container');
+
+// Request the current connection status and handle updates from the background.
+browser.runtime.onMessage.addListener((request) => {
+  if (request.channel === 'connectionStatus') {
+    ['disconnected', 'connected', 'error'].forEach((status) => {
+      statusContainerDiv.classList.remove(status);
+    });
+    statusContainerDiv.classList.add(request.connectionStatus);
+  }
+});
+browser.runtime.sendMessage({
+  channel: 'connectionStatusRequest',
+});
 
 
 connectButton.addEventListener('click', () => {
-  document.body.innerHTML += 'click!';
   browser.runtime.sendMessage({
     channel: 'connectionRequest',
     host: hostInput.value,
