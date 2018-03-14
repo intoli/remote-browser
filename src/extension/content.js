ef7da4efc067629bef4fc8ed3e15d0a154fd34e4
Evan Sangaline <esangaline@gmail.com>
2018-03-14 13:48:18 -0400

    Use a persistent connection to the content script contexts.

--- src/extension/content.js
+++ src/extension/content.js
@@ -1,9 +1,12 @@
-browser.runtime.onMessage.addListener((request) => {
-  if (request.channel === 'evaluateInContent') {
-    const { asyncFunction, args } = request;
+const backgroundPort = browser.runtime.connect({ name: 'contentScriptConnection' });
+
+backgroundPort.onMessage.addListener(({ id, message }) => {
+  if (message.channel === 'evaluateInContent') {
+    const { asyncFunction, args } = message;
     // eslint-disable-next-line no-eval
-    return Promise.resolve(eval(`(${asyncFunction}).apply(null, ${JSON.stringify(args)})`))
-      .catch(console.error);
+    Promise.resolve(eval(`(${asyncFunction}).apply(null, ${JSON.stringify(args)})`))
+      .then((result) => {
+        backgroundPort.postMessage({ id, message: result });
+      });
   }
-  return null;
 });
