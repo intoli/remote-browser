b979c33d1a6233d5223d4beafcc6f4ec66f862cd
Evan Sangaline <esangaline@gmail.com>
2018-03-15 12:04:38 -0400

    Catch and serialize errors during content script evaluation.

--- src/extension/content.js
+++ src/extension/content.js
@@ -1,12 +1,20 @@
+import { RemoteError } from '../errors';
+
+
 const backgroundPort = browser.runtime.connect({ name: 'contentScriptConnection' });
 
 backgroundPort.onMessage.addListener(({ id, message }) => {
   if (message.channel === 'evaluateInContent') {
     const { asyncFunction, args } = message;
-    // eslint-disable-next-line no-eval
-    Promise.resolve(eval(`(${asyncFunction}).apply(null, ${JSON.stringify(args)})`))
-      .then((result) => {
-        backgroundPort.postMessage({ id, message: result });
+    Promise.resolve()
+      // eslint-disable-next-line no-eval
+      .then(() => eval(`(${asyncFunction}).apply(null, ${JSON.stringify(args)})`))
+      .then(result => backgroundPort.postMessage({ id, message: result }))
+      .catch((error) => {
+        backgroundPort.postMessage({
+          id,
+          error: JSON.stringify((new RemoteError(error)).toJSON()),
+        });
       });
   }
 });
