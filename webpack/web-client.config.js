a63f2187ebaced7e6bd4049a87fd55612b184c45
Evan Sangaline <esangaline@gmail.com>
2018-03-20 20:23:40 -0400

    Use `null-loader` on the blacklisted web dependencies.

--- webpack/web-client.config.js
+++ webpack/web-client.config.js
@@ -3,6 +3,13 @@ const path = require('path');
 const webpack = require('webpack');
 
 
+// We'll explicitly whitelist the dependencies that we actually want to include.
+const packageJson = require(path.resolve(__dirname, '..', 'package.json'));
+const whitelistedDependencies = ['simple-websocket'];
+const blacklistedDependencies = Object.keys(packageJson.dependencies)
+  .filter(packageName => !whitelistedDependencies.includes(packageName));
+
+
 const options = {
   entry: {
     index: path.resolve(__dirname, '..', 'src', 'index.js'),
@@ -14,13 +21,6 @@ const options = {
     libraryTarget: 'umd',
     umdNamedDefine: true,
   },
-  externals: [
-    'express',
-    'express-ws',
-    'portfinder',
-    'selenium-webdriver',
-    'ws',
-  ],
   module: {
     rules: [
       {
@@ -34,6 +34,10 @@ const options = {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      ...blacklistedDependencies.map(dependency => ({
+        test: new RegExp(`^${path.resolve(__dirname, '..', 'node_modules', dependency)}/`),
+        loader: 'null-loader',
+      })),
     ],
   },
   plugins: [
