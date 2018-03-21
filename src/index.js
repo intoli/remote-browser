f0bee9dccb3e7660da1f829333127bc9b7afac6d
Evan Sangaline <esangaline@gmail.com>
2018-03-21 14:50:35 -0400

    Export the connection proxy from `index.js`.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Browser from './browser';
+import ConnectionProxy from './connections/proxy';
 
 export default Browser;
 export { Browser };
+export { ConnectionProxy };
 export * from './errors';
