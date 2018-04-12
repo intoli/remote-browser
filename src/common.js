37d8a6f52dfa669a624cde1fae0a9e518d2aaab5
Evan Sangaline <esangaline@gmail.com>
2018-04-12 10:29:14 -0400

    Add a method for serializing functions which works around the Safari bug.

--- src/common.js
+++ src/common.js
@@ -1,5 +1,24 @@
 import { TimeoutError } from './errors';
 
+
+export const serializeFunction = (f) => {
+  const serialized = f.toString();
+
+  // Safari serializes async arrow functions with an invalid `function` keyword.
+  // This needs to be removed in order for the function to be interpretable.
+  const safariPrefix = 'async function ';
+  if (serialized.startsWith(safariPrefix)) {
+    const arrowIndex = serialized.indexOf('=>');
+    const bracketIndex = serialized.indexOf('{');
+    if (arrowIndex > -1 && (bracketIndex === -1 || arrowIndex < bracketIndex)) {
+      return `async ${serialized.slice(safariPrefix.length)}`;
+    }
+  }
+
+  return serialized;
+};
+
+
 export class TimeoutPromise extends Promise {
   constructor(executor, timeout = 30000) {
     super((resolve, revoke) => {
